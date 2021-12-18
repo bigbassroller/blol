2fc20419029d2e939ef4220ce0e8e9d02079ca4a
Michael Chavez <mchavez1@space-rocket.com>
2021-12-17 23:50:12 -0800

    remove unnneeded comment and changed to more security bot friendly message

--- config/config.exs
+++ config/config.exs
@@ -1,18 +1,10 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
-# Configures the endpoint
 config :blol,
   riot_api_key: System.get_env("RIOT_API_KEY") ||
       raise """
       environment variable RIOT_API_KEY is missing.
-      For example: "RGAPI-f21a6b8d-7df4-464f-a4de-c880466abbc3".
+      For example: export RIOT_API_KEY="YOUR-RIOT-API-KEY-GOES-HERE".
       Get one here https://developer.riotgames.com/
       """
 
